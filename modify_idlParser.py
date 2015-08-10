aabb9044565fcdc9e117b63fcac287b40b9fd0b2
Natsuko Akaike <natsukoa@google.com>
2015-08-10 15:39:46 +0900

    output interface name and node properties

--- modify_idlParser.py
+++ modify_idlParser.py
@@ -11,47 +11,30 @@ sys.path.insert(0, blink_bindings_path)
 
 from blink_idl_parser import parse_file, BlinkIDLParser
 
-def GetPath(arg):
-    for dpath, dnames, fnames in os.walk(arg):    
-        path=[]
+def GetIDLFiles(dir):
+    for dpath, dnames, fnames in os.walk(dir):
         for fname in fnames:
-            if fname.endswith('.idl') and not fname=='InspectorInstrumentation.idl':
-                path.append(dpath+'/'+fname)
-        if path:
-            yield path
+            if fname.endswith('.idl') and fname!='InspectorInstrumentation.idl':
+                yield os.path.join(dpath, fname)
 
-def name_interface_list(child):
-    name_interface=[]
-    if child.GetLogLine('partial'):
-        pass
-    elif child.GetClass()=='Interface':
-        name_interface.append(child.GetName())
-    if name_interface:
-        print  name_interface
-
-def _dump_tree(node,args, depth=0):
-    #print '  ' * depth + str(node)
-    name_interface=[]
-    for child in node.GetChildren():
-        #name_interface_list(child)
-        #pdb.set_trace()
-        #print child.Traverse('partical',child.GetName())#I don't know how to find 'partical' object? node? attribute?
-        if child.GetClass()=='Interface':# and child.GetLogLine('partial'):
-            child.GetLogLine('partical')
-            name_interface.append(child.GetName())
-        #_dump_tree(child, depth + 1)
-    #if len(name_interface)>=2:
-    if name_interface:
-       print name_interface
+def GetInterfaceNodes(dir_path):
+    parser = BlinkIDLParser(debug=False)
+    for file in GetIDLFiles(dir_path):
+        definitions = parse_file(parser, file)
+        for definition in definitions.GetChildren():
+            if definition.GetClass() == 'Interface':
+                yield definition
 
 def main(args):
     parser = BlinkIDLParser(debug=False)
-    definitions = parse_file(parser, args[0])
-    #print '=== Tree ==='
-    _dump_tree(definitions,args[0])
-    
+    path = args[0]
+    count = 0
+    for interfaceNode in GetInterfaceNodes(path):
+        count += 1
+        print interfaceNode.GetName()
+        print interfaceNode.GetProperties()
+    #print 'count: ', count
 
 if __name__ == '__main__':
-    fnames=GetPath(sys.argv[1])
-    for files in fnames:
-        main(files)
+    main(sys.argv[1:])
+   
