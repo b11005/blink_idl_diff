1a84a7fb09924fecdf8816db7f4898bf4a60795e
Natsuko Akaike <natsukoa@google.com>
2015-08-19 19:58:30 +0900

    add extattribute

--- modify_idlParser.py
+++ modify_idlParser.py
@@ -42,25 +42,45 @@ def partial(interface_node_list):
         if interface_node.GetProperty('Partial', default=False):
             yield interface_node  
 
+
 def non_partial(interface_node_list):
     for interface_node in interface_node_list:
         if not interface_node.GetProperty('Partial', default=False):
             yield interface_node
+
 
 def getAttribute(interface_node):
     for attribute in interface_node.GetListOf('Attribute'):
         yield attribute
 
 
-def getType(node):
-    yield node.GetListOf('Type')[0].GetChildren()[0].GetName()
+def get_type(node):
+    return node.GetListOf('Type')[0].GetName()
+
+
+def get_undertype(node):
+    return node.GetListOf('Type')[0].GetChildren()[0].GetName()
+
+
+def get_extattirbute(interface_node):
+    for extattributes in interface_node.GetListOf('ExtAttributes'):
+        for extattribute_list in extattributes.GetChildren():
+            yield extattribute_list
+
+
+def extattr_dict(extattribute_list):
+    for extattribute in extattribute_list:
+        extattribute_dict = {}
+        extattribute_dict['Name'] = extattribute.GetName()
+        return extattribute_dict
 
 
 def attribute_dict(interface_node):
     for attribute in getAttribute(interface_node):
         attr_dict = {}
         attr_dict['Name'] = attribute.GetName()
-        attr_dict['Type'] = [attr_type for attr_type in getType(attribute)]
+        attr_dict['Typeref'] = get_undertype(attribute)
+        #attr_dict['ExtAttribute'] = [attr.GetName() for attr_list in get_extattirbute(attribute) for attr in attr_list]
         yield attr_dict
 
 def getOperation(interface_node):
@@ -68,19 +88,34 @@ def getOperation(interface_node):
         yield operation
 
 
-def getArgument(operation):
-    #for operation in getOperation(interface_node):
+def get_argument(operation):
+    #for argument in getOperation(operation):
     argument_node = operation.GetListOf('Arguments')[0]
     yield argument_node.GetListOf('Argument')
+
+
+def argument_dict(argument):
+    for arg_list in get_argument(argument):
+        for arg_name in arg_list:
+            arg_dict = {}
+            arg_dict['Name'] = arg_name.GetName()
+            arg_dict['Type'] = get_type(arg_name)
+            arg_dict['PrimitiveType'] = get_undertype(arg_name)
+            yield arg_dict
+
 
 def operation_dict(interface_node):
     for operation in getOperation(interface_node):
         operate_dict = {}
         operate_dict['Name'] = operation.GetName()
-        operate_dict['Argument'] = [argument.GetName() for argument_list in getArgument(operation) for argument in argument_list]
-        operate_dict['Type'] = [operate_type for operate_type in getType(operation)]
+        operate_dict['Arguments'] =[argument for argument in argument_dict(operation)]
+        operate_dict['Type'] = get_type(operation)
         yield operate_dict
 
+
+def get_const(interface_node):
+    for const in interface_node.GetListOf('Const'):
+        yield const
 
 def make_interface_dict(interface_node):
         interface_dict = {}
@@ -88,6 +123,7 @@ def make_interface_dict(interface_node):
         interface_dict['FilePath'] = get_filepath(interface_node)
         interface_dict['Attribute'] = [attr_name for attr_name in attribute_dict(interface_node)]
         interface_dict['Operation'] = [operation for operation in operation_dict(interface_node)]
+        interface_dict['ExtAttributes'] = extattr_dict(get_extattirbute(interface_node))
         return interface_dict
 
 
@@ -106,11 +142,11 @@ def main(args):
     partial_or_nonpartial = non_partial
     for interface_node in partial_or_nonpartial(getInterfaceNodes(path)):
         dictionary = make_interface_dict(interface_node)
-        #make_jsonfile(dictionary)
-        #make_interface_dict(interface_node)
+        make_jsonfile(dictionary)
+        make_interface_dict(interface_node)
         print make_interface_dict(interface_node)
-        #print [attr for attr in attribute_dict(interface_node)]
-        #print [ope for ope in operation_dict(interface_node)]
+        #print [con.GetName() for con in get_const(interface_node)]
+        #print operation_dict(interface_node)
 
 
 if __name__ == '__main__':
