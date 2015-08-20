770f85b37afa69d00d0b418fe289508008be78b0
Natsuko Akaike <natsukoa@google.com>
2015-08-20 16:47:00 +0900

    print getter, setter and deleter

--- modify_idlParser.py
+++ modify_idlParser.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os, sys, pdb, json
+import os, sys, json
 
 chromium_path = os.path.abspath(
     os.path.join(os.environ['HOME'], 'chromium', 'src'))
@@ -10,7 +10,7 @@ sys.path.insert(0, blink_bindings_path)
 
 from blink_idl_parser import parse_file, BlinkIDLParser
 
-def getIDLFiles(dir):
+def get_idl_files(dir):
     file_type='.idl'
     non_idl_set = (
         'InspectorInstrumentation.idl',
@@ -21,11 +21,11 @@ def getIDLFiles(dir):
                 yield os.path.join(dir_path, file_name)
 
 
-def getInterfaceNodes(dir_path):
+def get_interface_nodes(dir_path):
     parser = BlinkIDLParser(debug=False)
     class_name = 'Interface' 
-    for file in getIDLFiles(dir_path):
-        definitions = parse_file(parser, file)
+    for node_path in get_idl_files(dir_path):
+        definitions = parse_file(parser, node_path)
         for definition in definitions.GetChildren():
             if definition.GetClass() == class_name:
                 yield definition
@@ -49,13 +49,13 @@ def non_partial(interface_node_list):
             yield interface_node
 
 
-def getAttribute(interface_node):
+def get_attribute(interface_node):
     for attribute in interface_node.GetListOf('Attribute'):
         yield attribute
 
 
 def get_type(node):
-    return node.GetListOf('Type')[0].GetName()
+    return node.GetListOf('Type')[0].GetChildren()[0].GetName()
 
 
 def get_primitivetype(node):
@@ -64,31 +64,19 @@ def get_primitivetype(node):
         return under_type.GetName()
 
 
-def get_typeref(node):
-    under_type = node.GetListOf('Type')[0].GetChildren()[0]
-    if under_type.GetClass() == 'Typeref':
-        return under_type.GetName()
-
-
-def get_extattirbute(interface_node):
-    for extattributes in interface_node.GetListOf('ExtAttributes'):
-        for extattribute_list in extattributes.GetChildren():
-            yield extattribute_list
-
-
 def extattr_dict(extattribute_list):
     for extattribute in extattribute_list:
         extattribute_dict = {}
-        extattribute_dict['Extattribute Name'] = extattribute.GetName()
-        return extattribute_dict
+        extattribute_dict['Name'] = extattribute.GetName()
+        yield extattribute_dict
 
 
 def attribute_dict(interface_node):
-    for attribute in getAttribute(interface_node):
+    for attribute in get_attribute(interface_node):
         attr_dict = {}
         attr_dict['Name'] = attribute.GetName()
-        attr_dict['Typeref'] = get_typeref(attribute)
-        attr_dict['ExtAttributes'] = extattr_dict(get_extattirbute(attribute))
+        attr_dict['Type'] = get_type(attribute)
+        attr_dict['ExtAttribute'] = [extattr for extattr in extattr_dict(get_extattirbute(attribute))]
         yield attr_dict
 
 def get_operation(interface_node):
@@ -105,19 +93,30 @@ def argument_dict(argument):
     for arg_list in get_argument(argument):
         for arg_name in arg_list:
             arg_dict = {}
-            arg_dict['Argument Name'] = arg_name.GetName()
+            arg_dict['Name'] = arg_name.GetName()
             arg_dict['Type'] = get_type(arg_name)
-            arg_dict['PrimitiveType'] = get_primitivetype(arg_name)
             yield arg_dict
 
+
+def getter_Setter_deleter(operation):
+    for operate in operation:
+        if operate.GetProperty('GETTER',default=None):
+            print '__getter__'
+            #print operate.GetProperties()
+        elif operate.GetProperty('SETTER',default=None):
+            print '__setter__'
+        elif operate.GetProperty('DELETER',default=None):
+            print '__deleter__'
+        else:
+            print operate.GetName()
 
 def operation_dict(interface_node):
     for operation in get_operation(interface_node):
         operate_dict = {}
         operate_dict['Name'] = operation.GetName()
-        operate_dict['Arguments'] =[argument for argument in argument_dict(operation)]
+        operate_dict['Argument'] =[argument for argument in argument_dict(operation)]
         operate_dict['Type'] = get_type(operation)
-        operate_dict['ExtAttributes'] = extattr_dict(get_extattirbute(operation))
+        operate_dict['ExtAttributes'] = [extattr for extattr in extattr_dict(get_extattirbute(operation))]
         yield operate_dict
 
 
@@ -125,13 +124,14 @@ def get_const(interface_node):
     for const in interface_node.GetListOf('Const'):
         yield const
 
+
 def format_interface_dict(interface_node):
         interface_dict = {}
         interface_dict['Interface Name'] = interface_node.GetName()
         interface_dict['FilePath'] = get_filepath(interface_node)
         interface_dict['Attribute'] = [attr_name for attr_name in attribute_dict(interface_node)]
         interface_dict['Operation'] = [operation for operation in operation_dict(interface_node)]
-        interface_dict['ExtAttributes'] = extattr_dict(get_extattirbute(interface_node))
+        interface_dict['ExtAttributes'] = [extattr for extattr in extattr_dict(get_extattirbute(interface_node))]
         return interface_dict
 
 
@@ -143,16 +143,15 @@ def export_jsonfile(dictionary):
     f.close()
 
 
-
 def main(args):
     path = args[0]
     partial_or_nonpartial = non_partial
-    for interface_node in partial_or_nonpartial(getInterfaceNodes(path)):
-        dictionary = format_interface_dict(interface_node)
-        export_jsonfile(dictionary)
+    for interface_node in partial_or_nonpartial(get_interface_nodes(path)):
+        #dictionary = format_interface_dict(interface_node)
+        #export_jsonfile(dictionary)
         #print format_interface_dict(interface_node)
         #print [con.GetName() for con in get_const(interface_node)]
-
+        print getter_Setter_deleter(get_operation(interface_node))
 
 if __name__ == '__main__':
     main(sys.argv[1:])
