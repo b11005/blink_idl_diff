3288da3aca44ebeae18be585d981c349be4366b4
Natsuko Akaike <natsukoa@google.com>
2015-08-28 10:50:47 +0900

    print every idl filepath under directory (argument: directory >a.txt)

--- interface_node_path.py
+++ interface_node_path.py
@@ -2,14 +2,6 @@
 
 import os, sys
 
-chromium_path = os.path.abspath(
-    os.path.join(os.environ['HOME'], 'chromium', 'src'))
-blink_bindings_path = os.path.join(
-    chromium_path, 'third_party', 'WebKit', 'Source', 'bindings', 'scripts')
-sys.path.insert(0, blink_bindings_path)
-
-from blink_idl_parser import parse_file, BlinkIDLParser
-
 def get_idl_files(dir):
     file_type='.idl'
     non_idl_set = (
@@ -21,25 +13,10 @@ def get_idl_files(dir):
                 yield os.path.join(dir_path, file_name)
 
 
-def get_interface_nodes(dir_path):
-    parser = BlinkIDLParser(debug=False)
-    class_name = 'Interface' 
-    for idl_node in get_idl_files(dir_path):
-        node_path = idl_node
-        definitions = parse_file(parser, idl_node)
-        for definition in definitions.GetChildren():
-            if definition.GetClass() == class_name:
-                yield node_path
-
-
 def main(args):
     path = args[0]
-    filename = args[1]
-    interface_path_list = [node_path for node_path in get_interface_nodes(path)]
-    f = open(filename, 'w')
-    for interface_path in interface_path_list:
-        f.write(interface_path + '\t')
-    f.close()
+    for filepath  in get_idl_files(path):
+        print filepath
 
 
 if __name__ == '__main__':
