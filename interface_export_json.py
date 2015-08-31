e1d080c8cbda0313f5bd06739fc3344c98dd1a22
Natsuko Akaike <natsukoa@google.com>
2015-08-31 09:37:51 +0900

    argument: [1]directory_pass file [2]json file name

--- interface_export_json.py
+++ interface_export_json.py
@@ -10,25 +10,28 @@ sys.path.insert(0, blink_bindings_path)
 
 from blink_idl_parser import parse_file, BlinkIDLParser
 
-def get_idl_files(dir):
-    file_type='.idl'
-    non_idl_set = (
-        'InspectorInstrumentation.idl',
-    )
-    for dir_path, dir_names, file_names in os.walk(dir):
-        for file_name in file_names:
-            if file_name.endswith(file_type) and file_name not in non_idl_set:
-                yield os.path.join(dir_path, file_name)
+def load_filepath(path_file):
+    with open(path_file, 'r') as f:
+        return f.read()
 
 
-def get_interface_nodes(dir_path):
+def get_interfaces(file_path):
+    parser = BlinkIDLParser(debug=False)
+    class_name = 'Interface' 
+    definitions = parse_file(parser, file_path)
+    for definition in definitions.GetChildren():
+            if definition.GetClass() == class_name:
+                return definition
+
+
+'''def get_interface_nodes(dir_path):
     parser = BlinkIDLParser(debug=False)
     class_name = 'Interface' 
     for node_path in get_idl_files(dir_path):
         definitions = parse_file(parser, node_path)
         for definition in definitions.GetChildren():
             if definition.GetClass() == class_name:
-                yield definition
+                yield definition'''
 
 
 def get_filepath(interface_node):
@@ -38,15 +41,15 @@ def get_filepath(interface_node):
 
 
 def get_partial(interface_node_list):
-    for interface_node in interface_node_list:
-        if interface_node.GetProperty('Partial', default=False):
-            yield interface_node
+    #for interface_node in interface_node_list:
+    if interface_node.GetProperty('Partial', default=False):
+        return interface_node
 
 
-def get_non_partial(interface_node_list):
-    for interface_node in interface_node_list:
-        if not interface_node.GetProperty('Partial', default=False):
-            yield interface_node
+def get_non_partial(interface_node):
+    #for interface_node in interface_node_list:
+    if not interface_node.GetProperty('Partial', default=False):
+        return interface_node
 
 
 def get_attributes(interface_node):
@@ -111,7 +114,7 @@ def operation_dict(interface_node):
     operate_dict = {}
     for operation in get_operations(interface_node):
         operate_dict['Name'] = get_operation_name(operation)
-        operate_dict['Argument'] = [argument_dict(operation)]
+        operate_dict['Argument'] = argument_dict(operation)
         operate_dict['Type'] = get_type(operation)
         operate_dict['ExtAttributes'] = extattr_dict(operation)
     return operate_dict
@@ -149,24 +152,24 @@ def format_interface_dict(interface_node):
     return interface_dict
 
 
-def merge_partial_interface(interface_dict_list, partial_dict_list):
+def merge_partial_interface(interface_dict, partial_dict_list):
     for partial in partial_dict_list:
-        for interface in interface_dict_list:
-            if interface['Name'] == partial['Name']:
-                interface['Attribute'].append(partial['Attribute'])
-                interface['Operation'].append(partial['Operation'])
-                interface['ExtAttributes'].append(partial['ExtAttributes'])
-                interface.setdefault('Partial_FilePath', []).append(partial['FilePath'])
-                if interface['Constant']:
-                    interface.setdefault('Constant', []).append(partial['Constant'])
-    return interface_dict_list
+        #for interface in interface_dict_list:
+        if interface_dict['Name'] == partial['Name']:
+            interface_dict['Attribute'].append(partial['Attribute'])
+            interface_dict['Operation'].append(partial['Operation'])
+            interface_dict['ExtAttributes'].append(partial['ExtAttributes'])
+            interface_dict.setdefault('Partial_FilePath', []).append(partial['FilePath'])
+            if interface_dict['Constant']:
+                interface_dict.setdefault('Constant', []).append(partial['Constant'])
+    return interface_dict
 
 
-def format_dictionary(dictionary_list):
+'''def format_dictionary(dictionary_list):
     dictionary = {}
     for interface_dict in dictionary_list:
         dictionary.setdefault(interface_dict['Name'],interface_dict)
-    return dictionary
+    return dictionary'''
 
 
 def export_jsonfile(dictionary, json_file):
@@ -179,15 +182,31 @@ def export_jsonfile(dictionary, json_file):
 
 def main(args):
     path = args[0]
-    json_file = args[1]
-    interface_dict_list = [format_interface_dict(interface_node) for interface_node in get_non_partial(get_interface_nodes(path))]
-    partial_dict_list = [format_interface_dict(interface_node) for interface_node in get_partial(get_interface_nodes(path))]
-    dictionary_list = merge_partial_interface(interface_dict_list, partial_dict_list)
-    dictionary = format_dictionary(dictionary_list)
-    export_jsonfile(dictionary, json_file)
-    for i in dictionary_list:
-        print i
-    #print dictionary_list
+    path_file = args[1]
+    jsonfile_name = args[2]
+    d = {}
+    partial_dict_list = []
+    for filepath in load_filepath(path_file).split():
+        interface_node = get_interfaces(filepath)
+        if not interface_node:
+            pass
+        elif get_non_partial(interface_node):
+            name = interface_node.GetName()
+            if get_non_partial(interface_node):
+                d[name] = format_interface_dict(interface_node)
+        else:
+            format_interface_dict(interface_node)
+    export_jsonfile(d, jsonfile_name)
+    #merge_partial_interface(d, partial_interface_list)
+    #for k,v in d.items():
+        #interface_dict_list = [format_interface_dict(interface_node) for interface_node in get_non_partial(filepath)]
+        #partial_dict_list = [format_interface_dict(interface_node) for interface_node in get_partial(interface_node)]
+        #print partial_dict_list
+        #dictionary_list = merge_partial_interface(interface_dict_list, partial_dict_list)
+        #dictionary = format_dictionary(dictionary_list)
+        #export_jsonfile(dictionary, json_file)
+        #for i in dictionary_list:
+            #print i
 
 
 if __name__ == '__main__':
