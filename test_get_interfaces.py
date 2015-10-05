d59ff235968429889edf39e56219a1d4cfe92246
Natsuko Akaike <natsukoa@google.com>
2015-10-05 18:43:50 +0900

    write down all test units

--- test_get_interfaces.py
+++ test_get_interfaces.py
@@ -17,13 +17,6 @@ class TestFunctions(unittest.TestCase):
            self.assertEqual(str(type(actual)), "<class 'idl_parser.idl_node.IDLNode'>")
 
 
-    def test_interfaces(self):
-        interface_nodes = [collect_idls_into_json.is_interface_node(actual) for actual in collect_idls_into_json.get_definitions(utilities.read_file_to_list(_FILE))]
-        for interface in interface_nodes:
-            if interface:
-                self.assertEqual(collect_idls_into_json.get_interface_node(actual).GetClass(), 'Interface')
-
-
     def test_implements(self):
         implements = [collect_idls_into_json.is_implements(actual) for actual in collect_idls_into_json.get_definitions(utilities.read_file_to_list(_FILE))]
         for implement in implements:
@@ -33,14 +26,20 @@ class TestFunctions(unittest.TestCase):
                 self.assertFalse(implement)
 
 
-    def test_fileter_partial(self):
-        for actual in collect_idls_into_json.filter_partial(collect_idls_into_json.get_definitions(utilities.read_file_to_list(_FILE))):
-            self.assertTrue(actual.GetProperty('Partial'))
-
-
     def test_fileter_non_partial(self):
-        for actual in collect_idls_into_json.filter_non_partial(collect_idls_into_json.get_definitions(utilities.read_file_to_list(_FILE))):
-            self.assertFalse(actual.GetProperty('Partial'))
+        for node in collect_idls_into_json.get_definitions(utilities.read_file_to_list(_FILE)):
+            if node.GetClass() == 'Interface' and not node.GetProperty('Partial'):
+                self.assertTrue(collect_idls_into_json.filter_non_partial(node))
+            else:
+                self.assertFalse(collect_idls_into_json.filter_non_partial(node))
+
+
+    def test_fileter_partial(self):
+        for node in collect_idls_into_json.get_definitions(utilities.read_file_to_list(_FILE)):
+            if node.GetClass() == 'Interface' and node.GetProperty('Partial'):
+                self.assertTrue(collect_idls_into_json.filter_partial(node))
+            else:
+                self.assertFalse(collect_idls_into_json.filter_partial(node))
 
 
     def test_get_filepaths(self):
@@ -50,44 +49,92 @@ class TestFunctions(unittest.TestCase):
             self.assertTrue(path.endswith('.idl'))
 
 
-    def test_get_const_node(self):
+    def test_get_const_node_list(self):
         for actual in collect_idls_into_json.get_definitions(utilities.read_file_to_list(_FILE)):
-            for const in collect_idls_into_json.get_const_node(actual):
+            for const in collect_idls_into_json.get_const_node_list(actual):
                 if const:
                     self.assertEqual(const.GetClass(), 'Const')
 
 
-    def test_const_type(self):
+    '''def test_const_type(self):
         for actual in collect_idls_into_json.get_definitions(utilities.read_file_to_list(_FILE)):
             pass
-            #print collect_idls_into_json.get_const_type(actual)
-                    #self.assertEqual(const.GetClass(), 'Const')
 
 
     def test_get_const_value(self):
         for actual in collect_idls_into_json.get_definitions(utilities.read_file_to_list(_FILE)):
             pass
-            #print collect_idls_into_json.get_const_value(actual)
 
 
-    def test_get_attribute_node(self):
+    def test_const_node_to_dict(self):
+    '''
+
+
+    def test_get_attribute_node_list(self):
         for actual in collect_idls_into_json.get_definitions(utilities.read_file_to_list(_FILE)):
-            for attribute in collect_idls_into_json.get_attribute_node(actual):
+            for attribute in collect_idls_into_json.get_attribute_node_list(actual):
                 if attribute:
                     self.assertEqual(attribute.GetClass(), 'Attribute')
 
+    '''def test_get_attribute_type(self):
+        pass
 
-    def test_get_operation_node(self):
+
+    def test_attribute_node_to_dict(self):
+        pass'''
+
+
+    def test_get_operation_node_list(self):
         for actual in collect_idls_into_json.get_definitions(utilities.read_file_to_list(_FILE)):
-            for operation in collect_idls_into_json.get_operation_node(actual):
+            for operation in collect_idls_into_json.get_operation_node_list(actual):
                 if operation:
                     self.assertEqual(operation.GetClass(), 'Operation')
 
 
-    '''def test_interface_to_dict(self):
-        for actual in collect_idls_into_json.get_interfaces(utilities.read_file_to_list('sample0.txt')):
-            self.assertTrue(key_set.issuperset(collect_idls_into_json.interface_to_dict(actual)))'''
+    def test_get_argument_node_list(self):
+        pass
 
+    def test_argument_node_to_dict(self):
+        pass
+
+    def test_get_operation_name(self):
+        pass
+
+
+    def test_operation_node_to_dict(self):
+        pass
+
+
+    def test_get_extattribute_node_list(self):
+        for actual in collect_idls_into_json.get_definitions(utilities.read_file_to_list(_FILE)):
+            for extattr in collect_idls_into_json.get_extattribute_node_list(actual):
+                if extattr:
+                    self.assertEqual(extattr.GetClass(), 'ExtAttribute')
+
+    def test_extattr_node_to_dict(self):
+        pass
+
+    def test_get_inherit_node(self):
+        pass
+
+    def test_inherit_node_to_dict(self):
+        pass
+
+
+    def test_interface_to_dict(self):
+        for actual in collect_idls_into_json.get_definitions(utilities.read_file_to_list('sample0.txt')):
+            self.assertTrue(_KEY_SET.issuperset(collect_idls_into_json.interface_node_to_dict(actual)))
+
+
+    def test_merge_partial_dict(self):
+        pass
+
+
+    def test_merge_implement_node(self):
+        pass
+
+    def test_export_to_jsonfile(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
